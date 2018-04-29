053e829235caf7e9fdbc00dfcb47b739b1cf89e8
unknown <beaudelin.universite@gmail.com>
2018-04-29 16:57:31 +0200

    initial commité

---
+++ Jewel32bits/src/JewelMidlet32bits/JewelGame.java
@@ -0,0 +1,45 @@
+package JewelMidlet32bits;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public class JewelGame extends MIDlet implements CommandListener{
+	
+    Display display;
+    private JewelCanvas canvas;
+    private Command exitCommand = new Command("Exit", Command.EXIT, 60);
+    
+    public JewelGame(){
+    	display = Display.getDisplay(this);
+    	canvas = new JewelCanvas(this);
+    }
+    
+	protected void destroyApp(boolean arg0){
+		// TODO Auto-generated method stub
+		display.setCurrent(null);
+	}
+
+	protected void pauseApp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void startApp() throws MIDletStateChangeException {
+		// TODO Auto-generated method stub
+		canvas.init();
+		canvas.addCommand(exitCommand);
+		canvas.setCommandListener(this);
+		
+		display.setCurrent(canvas);
+	}
+
+	public void commandAction(Command c, Displayable s) {
+		// TODO Auto-generated method stub
+		if (c == exitCommand) {
+		    destroyApp(false);
+		    notifyDestroyed();
+		}
+	}
+
+}
